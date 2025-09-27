6b400ed4a276e172ecd4a69de57690c5eb2d5613
HoniBrodo <matthewdheale@gmail.com>
2025-09-27 21:12:20 +0100

    added teamster loadout

--- src/player/Teamster.cpp
+++ src/player/Teamster.cpp
@@ -3,6 +3,11 @@
 Teamster::Teamster() : BaseClass("Teamster") {
 
     classBio = "The backbone of any mission, the Teamster is a hardened labourer whos seen it all; long hauls through deep space, busted equipment in zero G, and bosses who never cared if they lived or died. Used to pulling double shifts and keeping crews together, the Teamster has grit in spades.";
+
+    loadout01Items = { "Vaccsuit (AP 3)", "Revolver (6 rounds)", "Crowbar", "Flashlight"};
+    loadout02Items = { "Heavy Duty Work Clothes (AP 2)", "Explosives & Detonator", "Cigarettes" };
+    loadout03Items = { "Standard Crew Attire (AP 1)", "Nail Gun(32 rounds)", "HeadLamp", "Toolbelt with Assorted Tools" };
+    loadout04Items = { "Heavy Duty Work Clothes (AP 2)", "Drill (as Assorted Tools)", "Paracord(100m)", "Salvage Drone" };
 }
 
 
