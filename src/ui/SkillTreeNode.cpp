30ca137b651bef0b4f0be926c8ae72cfb839ee52
HoniBrodo <matthewdheale@gmail.com>
2025-10-26 11:57:45 +0000

    created SkillTreeNode class

---
+++ src/ui/SkillTreeNode.cpp
@@ -0,0 +1,6 @@
+#include "SkillTreeNode.h"
+
+SkillTreeNode::SkillTreeNode()
+{
+
+}
---
+++ src/ui/SkillTreeNode.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+#include "../core/Renderer.h"
+#include "raylib.h" 
+
+class SkillTreeNode
+{
+public:
+    SkillTreeNode();
+
+private:
+
+};
